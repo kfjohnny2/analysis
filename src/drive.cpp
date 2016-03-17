9ce101c1819edfd4fe58f578d640c7e5f331a563
kfjohnny2 <kfjohnny@ufrn.edu.br>
2016-03-17 20:53:02 -0300

    100%

--- src/drive.cpp
+++ src/drive.cpp
@@ -23,6 +23,7 @@ int main(int argc, char const *argv[])
     int seed = 1;
     ofstream myfile_third;
     ofstream myfile_worst;
+    ofstream myfile_normal;
 
     /*VERIFY IF HAVE ANY ARGUMENTS ON COMMANDO LINE FOR THE VECTOR EXPONENT,
     IF YES PUT ON ARRAY SIZE, ELSE USES THE DEFAULT VALUE FOR THE EXPONENT*/
@@ -57,7 +58,7 @@ int main(int argc, char const *argv[])
     //------------------------------------------//
 
     // BUILD THE ARRAY OF FUNCTION WITH ALL SEARCHS
-    
+
 
     long int (*funcArray[])(vector<long int> , long int , long int , long int) = {
         sSearchRec,
@@ -76,49 +77,63 @@ int main(int argc, char const *argv[])
                            "binary_search_rec",
                            "ternary_search_ite",
                            "wrapper_binary_search" };
-    
+
     long int third;
     long int worst = 1001; //I'm using a vector with values from 0 to 1000, and 1001 dont exist
-    cout << "\n>>> Calculando Tempo de Algoritimos" << endl;
+    cout << "\n>>> TIME CALCULATE PROCESS WILL BEGIN NOW... " << endl;
+    vector<long int>::const_iterator begin;
+    vector<long int>::const_iterator last;
+
     for(int i = 0; i < 7; i++){
         if (i == 2){
             cout << "\n>>> Sorting vector" << endl;
             sort(V.begin(), V.end()); // sort the vector for sorted searchs
             cout << "\n>>> Sorting completed\n" << endl;
         }
-        
-        //int t;
+
+
         myfile_third.open("files/thirdFour/" + n_function[i] + "_thirdFour.dat"  );
         myfile_worst.open("files/worstCase/" + n_function[i] + "_worstCase.dat"  );
+        myfile_normal.open("files/normal/" + n_function[i] + "_normalCase.dat"  );
 
                 cout << n_function[i] << endl;
-        if(i == 0 && arraySize > pow(2, 13)){// VERIFIQUE O LIMITE DE ITERAÇÕES PARA A SEQUENCIAL RECURSIVA E ALTERE AQUI .. PQ DEPOIS DO LIMITE ELE PARA DE EXECUTAR 
+        if(i == 0 && arraySize > pow(2, 13)){// VERIFIQUE O LIMITE DE ITERAÇÕES PARA A SEQUENCIAL RECURSIVA E ALTERE AQUI .. PQ DEPOIS DO LIMITE ELE PARA DE EXECUTAR
             for(long int n = 32; n <= pow(2, 13); n *= 2){
                 third = V[3*n/4];
-                
-                cout << n << endl;
-                myfile_third << n << " " << calculateTime(V, funcArray[i], third, 0, n-1) << endl; //imprime o valor da variavel cont no dat    
-                myfile_worst << n << " " << calculateTime(V, funcArray[i], worst, 0, n-1) << endl; //imprime o valor da variavel cont no dat   
+                cout << "RUNNING " << n_function[i] <<" for the size [" << n << "]\n"<< endl;
+                begin = V.begin();
+                last = V.begin() + n;
+                vector<long int> new_arr(begin+1, last);
+                cout << "RUNNING THE THIRD PART\n"<< endl;
+                myfile_third << n << " " << calculateTime(new_arr, funcArray[i], third, 0, n-1) << endl; //imprime o valor da variavel cont no dat
+                cout << "RUNNING THE WORST CASE\n"<< endl;
+                myfile_worst << n << " " << calculateTime(new_arr, funcArray[i], worst, 0, n-1) << endl; //imprime o valor da variavel cont no dat
+                cout << "RUNNING THE NORMAL\n"<< endl;
+                myfile_normal << n << " " << calculateTime(new_arr, funcArray[i], 47, 0, n-1) << endl; //imprime o valor da variavel cont no dat
             }
-
-
         }else{
-
             for(long int n = 32; n <= arraySize; n *= 2){
                 third = V[3*n/4];
-                
-                //cout << n << endl;
-                myfile_third << n << " " << calculateTime(V, funcArray[i], third, 0, n-1) << endl; //imprime o valor da variavel cont no dat    
-                myfile_worst << n << " " << calculateTime(V, funcArray[i], worst, 0, n-1) << endl; //imprime o valor da variavel cont no dat   
+                cout << "RUNNING " << n_function[i] <<" for the size [" << n << "]\n"<< endl;
+                begin = V.begin();
+                last = V.begin() + n;
+                vector<long int> new_arr(begin+1, last);
+                cout << "RUNNING THE THIRD PART\n"<< endl;
+                myfile_third << n << " " << calculateTime(new_arr, funcArray[i], third, 0, n-1) << endl; //imprime o valor da variavel cont no dat
+                cout << "RUNNING THE WORST CASE\n"<< endl;
+                myfile_worst << n << " " << calculateTime(new_arr, funcArray[i], worst, 0, n-1) << endl; //imprime o valor da variavel cont no dat
+                cout << "RUNNING THE NORMAL\n"<< endl;
+                myfile_normal << n << " " << calculateTime(new_arr, funcArray[i], 47, 0, n-1) << endl; //imprime o valor da variavel cont no dat
             }
-        }    
+        }
 
         myfile_third.close();
-        myfile_worst.close(); 
-    }          
+        myfile_worst.close();
+        myfile_normal.close();
+    }
 
-    cout << "\n>>> Medições concluidas, Obrigado .." << endl;
-    
+    cout << "\n>>> Measures done, Sorry for the wait ..." << endl;
+
     V.clear();
     return EXIT_SUCCESS;
 }
